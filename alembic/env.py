63e23d92cce22d36867327c64bc04126fe6c9b0c
Xavier901 <39602489+Xavier901@users.noreply.github.com>
2025-04-25 16:06:26 +0530

    alembic database setup

--- alembic/env.py
+++ alembic/env.py
@@ -5,10 +5,13 @@ from sqlalchemy import pool
 
 from alembic import context
 
+from models import Base
+from config import setting
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-
+config.set_main_option("sqlalchemy.url",f'postgresql+psycopg2://{setting.database_username}:{setting.database_password}@{setting.database_hostname}:{setting.database_port}/{setting.database_name}')
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
@@ -18,7 +21,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
